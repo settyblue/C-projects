6053f19fccca2cef0a5bdd451951192c225c3556
Rakshith Kunchum <settyblue@gmail.com>
2015-10-28 21:45:24 -0700

    Added file for shortest super string but code not implemented.

---
+++ String Problems/ShortestSuperstring.cpp
@@ -0,0 +1,11 @@
+//Given a set of n strings arr[], find the smallest string that contains each string in the given set as substring. 
+//We may assume that no string in arr[] is substring of another string.
+//It is a NP hard problem.
+//We can only implement approximate Greedy Problem.
+//Read the example below:
+//Input:  arr[] = {"geeks", "quiz", "for"}
+//Output: geeksquizfor
+//Input:  arr[] = {"catg", "ctaagt", "gcta", "ttca", "atgcatc"}
+//Output: gctaagttcatgcatc
+
+
